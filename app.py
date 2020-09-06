de62568284fbeccb6d73a7316d2a2af73d59c363
johnson deng <johnson.jy.deng@gmail.com>
2020-09-05 19:16:08 -0700

    Added dummy data to passed into template and wrote jinja script to iterate over data and generate list items in the page

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)  #__name__ specifies that the application will take on the same name as the file.
 
 @app.route('/')
 def index():
-    render_template('index.html')
+    return render_template('index.html', data=[{
+        'description': 'Todo 1'
+    }, {
+        'description': 'Todo 2'
+    }, {
+        'description': 'Todo 3'
+    }, {
+        'description': 'Todo 4'
+    }])
