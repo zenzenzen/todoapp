46860f3284baacf35bd20717321e62b40276be87
johnson deng <johnson.jy.deng@gmail.com>
2020-09-05 19:03:18 -0700

    created html template and linked app script to the html.

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 from flask import Flask
-app = Flask(__name__) #__name__ specifies that the application will take on the same name as the file.
+app = Flask(__name__)  #__name__ specifies that the application will take on the same name as the file.
+
+@app.route('/')
+def index():
+    render_template('index.html')
