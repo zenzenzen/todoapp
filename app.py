bb84479e8aba95919a96abd8833d71db573940a0
johnson deng <johnson.jy.deng@gmail.com>
2020-09-07 15:58:54 -0700

    Bug fixes: typos and incorrect object names for Model and dbObject. Controller now works and updates from the model when data is inserted into Postgresql table.

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)  #__name__ specifies that the application will take on the same name as the file.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp'
+dbObject = SQLAlchemy(app) # Call in a db object that we can use to perform CRUD operations.
+
+
+
+class ToDo(dbObject.Model):
+    __tablename__ = 'todos'
+    id = dbObject.Column(dbObject.Integer, primary_key=True)
+    description = dbObject.Column(dbObject.String(), nullable=False)
+    # completed = dbObjectColumn(dbObjectBoolean, nullable=True)
+
+    def __repr__(self): #built-in reprint method for debugging
+        return f'<Todo {self.id} {self.description}'
+
+dbObject.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    }, {
-        'description': 'Todo 2'
-    }, {
-        'description': 'Todo 3'
-    }, {
-        'description': 'Todo 4'
-    }])
+    return render_template('index.html', data=ToDo.query.all()
+    )
+
+if __name__ == '__main__':
+    app.run()
