bcc55cdfae61c402a2bc1f9312a6a3134e9e220e
johnson deng <johnson.jy.deng@gmail.com>
2020-09-09 20:48:14 -0700

    Integrated Flask-Migrate removed db_create_all() and used flask_migrate to recreate the todos table. Afterwards, I added a column in the ToDo Model and ran 'flask db migrate + flask db upgrade' to finish out db changes

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 from flask import Flask, render_template, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import migrate
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)  #__name__ specifies that the application will take on the same name as the file.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp'
 dbObject = SQLAlchemy(app) # Call in a db object that we can use to perform CRUD operations.
 
-migrate = (Migrate, dbObject)
+migrate = Migrate(app, dbObject)
 
 
 class ToDo(dbObject.Model):
     __tablename__ = 'todos'
     id = dbObject.Column(dbObject.Integer, primary_key=True)
     description = dbObject.Column(dbObject.String(), nullable=False)
+    completed = dbObject.Column(dbObject.Boolean, nullable=False, default = False)
     # completed = dbObjectColumn(dbObjectBoolean, nullable=True)
 
     def __repr__(self): #built-in reprint method for debugging
         return f'<Todo {self.id} {self.description}'
 
-dbObject.create_all()
+# dbObject.create_all()   --> No longer required because we're using Flask-Migrate now.
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -51,18 +52,6 @@ def create_todo():
 def index():
     return render_template('index.html', data=ToDo.query.all()
     )
-
-@app.route('/todos/create', methods=['POST'])
-def create_todo():
-    newDescription = request.get_json()['description']
-    newItem = ToDo(description=newDescription)
-    dbObject.session.add(newItem)       #ignore the red underline
-    dbObject.session.commit()
-
-    #you should redirect after finishing... instead of returning the home view
-    return jsonify({
-        'description': newItem.description
-    })
 
 # if __name__ == '__main__':
 #     app.run()
