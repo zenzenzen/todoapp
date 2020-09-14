9bf0a5ee7ab1a69bb4a1a55d4d32dcb29950b04e
johnson deng <johnson.jy.deng@gmail.com>
2020-09-14 11:24:52 -0700

    Finally got Delete Working... Turns out that the DELETE function was pretty much fine as it is. I added an extra step by force an INT conversion from string. The thing that I seemed to get wrong was by having the ToDo.query.filter_by(id == deleteTag).delete() when it should actually be: ... filter_by(id = deleteTag).delete() ; It just seems that my weird C/C++ habit just surfaced by having that equivalence operator show up when a single = sign was correct. I know, facepalm.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, abort, jsonify, redirect, url_for, make_response
+from flask import Flask, render_template, request, abort, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)  #__name__ specifies that the application will take on the same name as the file.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 dbObject = SQLAlchemy(app) # Call in a db object that we can use to perform CRUD operations.
 
 migrate = Migrate(app, dbObject)
@@ -14,7 +15,7 @@ class ToDo(dbObject.Model):
     __tablename__ = 'todos'
     id = dbObject.Column(dbObject.Integer, primary_key=True)
     description = dbObject.Column(dbObject.String(), nullable=False)
-    completed = dbObject.Column(dbObject.Boolean, nullable=False, default=False)
+    completed = dbObject.Column(dbObject.Boolean, nullable=False)
     # completed = dbObjectColumn(dbObjectBoolean, nullable=True)
 
     def __repr__(self): #built-in reprint method for debugging
@@ -44,36 +45,37 @@ def create_todo():
     finally:
         dbObject.session.close()
     if error:
-        abort(500)
-        print(sys.exc_info())
+        abort(400)
     else:
         return jsonify(body)
 
-@app.route('/todos/<todo_id>/set-completed', methods=['PATCH'])
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
-    try:
-        completedTask = request.get_json()['completed']
-        print('completed', completedTask)
-        todoItem = ToDo.query.filter_by(id=todo_id).all()
-        todoItem.completed=True
-        dbObject.session.commit()
-    except:
-        dbObject.session.rollback()
-    finally:
-        dbObject.session.close()
-    return redirect(url_for('index'))
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = ToDo.query.get(todo_id)
+    todo.completed = completed
+    dbObject.session.commit()
+  except:
+    dbObject.session.rollback()
+  finally:
+    dbObject.session.close()
+  return redirect(url_for('index'))
 
-@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+@app.route('/todos/<todo_id>', methods=['DELETE'])  # Don't need to have additional '/delete' path after <todo_id>
 def delete_todo(todo_id):
     # validDelete = make_response(jsonify({}), 204)
+    deleteTag = int(todo_id)
     try:
-        Todo.query.filter_by(ToDo.id == todo_id).delete()
+        ToDo.query.filter_by(id = deleteTag).delete() # I had an extra = for "X == Y" 
+                                                      # Turns out maybe this is unnecessary
         dbObject.session.commit()
     except:
         dbObject.session.rollback()
     finally:
         dbObject.session.close()
-    return redirect(url_for('index'))
+        return jsonify({'success':True})
 
 @app.route('/')
 def index():
