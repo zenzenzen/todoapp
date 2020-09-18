feeb64295edc92fcfcfa412872969ca244983abc
johnson deng <johnson.jy.deng@gmail.com>
2020-09-17 19:22:58 -0700

    Naming conventions - bug prevention: I made a pretty frustrating mistake the last commit by having list_id be listID and running into issues with accessing the colum due to psql's naming convention defaults. I caught another issue since I unconsciously ended up using camel case with the todolist class, so I'm taking the opportunity to correct that mistake before it gets out of hand. Database has been migrate+upgraded and now there are no camel case column names in my data models. Whew.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ migrate = Migrate(app, dbObject)
 class ToDoList(dbObject.Model):
     __tablename__ = 'todolists'
     id = dbObject.Column(dbObject.Integer, primary_key=True)
-    listName = dbObject.Column(dbObject.String(), nullable=False)
+    name = dbObject.Column(dbObject.String(), nullable=False)
     todos = dbObject.relationship('ToDo', backref='lists', lazy=True)
 
 class ToDo(dbObject.Model):
@@ -22,7 +22,13 @@ class ToDo(dbObject.Model):
     id = dbObject.Column(dbObject.Integer, primary_key=True)
     description = dbObject.Column(dbObject.String(), nullable=False)
     completed = dbObject.Column(dbObject.Boolean, nullable=False)
-    listID = dbObject.Column(dbObject.Integer, dbObject.ForeignKey('todolists.id'))
+    list_id = dbObject.Column(dbObject.Integer, dbObject.ForeignKey('todolists.id'))
+    
+    # MAJOR NOTES TO SELF:
+    # Do not, for the love of Postgresql, have CAPITALIZED letters in the name of a table
+    # unless you want it "BETWEEN QUOTES" because all the queries are folded into lowercase 
+    # practically no matter what. listID became '.listid' no matter how many different ways I tried to 
+    # query the damn column.
 
     def __repr__(self): #built-in reprint method for debugging
         return f'<Todo {self.id} {self.description} {self.completed}'
@@ -31,13 +37,14 @@ class ToDo(dbObject.Model):
 
 # dbObject.create_all()   --> No longer required because we're using Flask-Migrate now.
 
-@app.route('/todos/create', methods=['POST'])
-def create_todo():
-    error = False
+@app.route('/todos/<list_id>/create', methods=['POST'])
+def create_todo(list_id):
+    parentList = int(list_id)
+    error = False 
     body = {}
     try:
         newDescription = request.get_json()['description']
-        newItem = ToDo(description=newDescription, completed=False)
+        newItem = ToDo(description=newDescription, completed=False, list_id=parentList) 
         dbObject.session.add(newItem)       
         dbObject.session.commit()    
         body['description'] = newItem.description   #  Circumvent expire_on_commit issues
@@ -50,6 +57,26 @@ def create_todo():
         dbObject.session.rollback()
         print(sys.exc_info())
         
+    finally:
+        dbObject.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+@app.route('/lists/create', methods=['POST']) #So far just copying what I have for the normal create.
+def create_list():
+    error = False
+    body = {}
+    try:
+        newListName = request.get_json()['name']
+        todolist = ToDoList(name = newListName)
+        dbObject.session.add(todolist)
+        dbObject.session.commit()
+    except:
+        error = True
+        dbObject.session.rollback()
+        print(sys.exc_info())
     finally:
         dbObject.session.close()
     if error:
@@ -71,6 +98,18 @@ def set_completed_todo(todo_id):
     dbObject.session.close()
   return redirect(url_for('index'))
 
+@app.route('/removelist/<parent_list_id>', methods=['DELETE'])
+def remove_list(parent_list_id):
+    bye_list = int(parent_list_id)
+    try:
+        ToDoList.query.filter_by(id = bye_list).delete()
+        dbObject.session.commit()
+    except:
+        dbObject.session.rollback()
+    finally:
+        dbObject.session.close()
+        return jsonify({'success': True})
+
 @app.route('/todos/<todo_id>', methods=['DELETE'])  # Don't need to have additional '/delete' path after <todo_id>
 def delete_todo(todo_id):
     # validDelete = make_response(jsonify({}), 204)
@@ -83,13 +122,17 @@ def delete_todo(todo_id):
         dbObject.session.rollback()
     finally:
         dbObject.session.close()
-        return jsonify({'success':True})
+        return jsonify({'success': True})
 
-@app.route('/lists/<list_id>')
-def index(list_id):
+@app.route('/lists/<parent_list_id>')
+def get_list_todos(parent_list_id):
     return render_template('index.html', 
-    data=ToDo.query.filter_by(list_id=ListID).order_by('id')
+    data=ToDo.query.filter_by(list_id = parent_list_id).order_by('id')
     .all()
+)
 
+@app.route('/')
+def index():
+    return redirect(url_for('get_list_todos', list_id = 1))
 # if __name__ == '__main__':
 #     app.run()
